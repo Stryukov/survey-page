190c061bc279da1422aacbe5cf7f976f8d990d6b
Stryukov <stryukov.semen@gmail.com>
2018-09-06 12:01:26 +1200

    fix bug

---
+++ lang/lang.EN.php
@@ -0,0 +1,21 @@
+<?php   
+  $lang['title']      = 'Welcome to Petropavlovsk-Kamchatsky';
+  $lang['welcome']  = 'Welcome to Petropavlovsk-Kamchatsky';
+  $lang['welcome-text']   = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in. ';
+  $lang['resource-title']      = 'Useful links';       
+  $lang['resource-subtitle']      = 'Lorem ipsum dolor sit amet';       
+  $lang['link1-title']      = 'Title';       
+  $lang['link1-desc']      = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.';       
+  $lang['link2-title']      = 'Title';       
+  $lang['link2-desc']      = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.';       
+  $lang['link3-title']      = 'Title';       
+  $lang['link3-desc']      = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.';       
+  $lang['link4-title']      = 'Title';       
+  $lang['link4-desc']      = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.';       
+  $lang['link5-title']      = 'Title';       
+  $lang['link5-desc']      = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.';       
+  $lang['link6-title']      = 'Title';       
+  $lang['link6-desc']      = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.';
+  $lang['copyright']      = 'photo by <a href="https://www.facebook.com/ikozlov82">Ivan Kozlov</a>';       
+
+?>
